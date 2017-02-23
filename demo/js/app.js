b54782f9fe317d99bb3a15d77405fa077ce594b0
wangpq <wpq163yx@163.com>
2017-02-23 18:52:53 +0800

    增加了示例页面的内容

--- demo/js/app.js
+++ demo/js/app.js
@@ -5,8 +5,72 @@ var gmPrdApp={
 		}
 	},
 	dzTabsFn : function(){
+	},
+	appraisalPage : function(num){
+		var tpl='';
+		for(var i=0;i<10;i++){
+			tpl=tpl+'<li style="height:100px;border-bottom:1px solid #ccc;">第'+num+'页第'+(i+1)+'条评论类容</li>';
+		}
+        $("#appraisalContent").html('<ul data-index="'+num+'">'+tpl+'</ul>');  
+	},
+	jumpToAppTop : function(){
+        $("body,html").animate({
+            scrollTop: $("#appraisalContent").offset().top-36
+        },80);
+	},
+	/* 根据浏览猜你喜欢 */
+	guessLickFn: function(data){
+		if (!(data.isSuccess=="Y" && data.size>0)){
+			$('#guessLike').hide();
+			return false;
+		}
+		var htm = '\
+			<div class="hd">\
+				<h3 class="title">根据浏览猜你喜欢</h3>\
+				<div id="btn-change" class="r opt"><span class="text">换一组</span><i class="i-refresh"></i></div>\
+			</div>\
+			<div class="bd">\
+				<div class="bd-cont">\
+					<ul class="pushul clearfix" style="width: 4654px; position: relative;">\
+						<% for(var i=0,j=lst.length; i<j; i++){ \
+							var prd = lst[i];\
+							var point = i<j-1?",":"";\
+							bigData(prd.productId+point);\
+						%>\
+						<li>\
+							<a track="2:<%= prd.pid %>" href="http:<%=prd.purl %>"  target="_blank" title="<%= prd.pn %>" maima_param="<%=prd.maima_param%>"><img width="130" height="130" src="http:<%= prd.iurl %>" alt="<%= prd.pn %>"/></a>\
+							<a track="2:<%= prd.pid %>" href="http:<%=prd.purl %>"  target="_blank" title="<%= prd.pn %>" maima_param="<%=prd.maima_param%>"><h2><%= prd.pn %></h2></a>\
+							<p class="yuan colprice fb">¥<span><%= prd.price %></span></p>\
+						</li>\
+						<% } %>\
+					</ul>\
+				</div>\
+			</div>\
+			\
+			';
+
+		var products = ""
+		  , tlscroll = $('body').hasClass('w990') ? 5 : 6;
+		template.helper('bigData', function (pid) { products += pid; });
+		$('#guessLike').addClass("gslider").attr('maima_param',data.maima_param).html(template.compile(htm)(data));
+
+		if (typeof trackEvent != "undefined") { trackEvent(2, products); }
+
+		if (data.size > tlscroll) {
+			$('#guessLike .bd-cont').gSlider({
+				isAuto: false,
+				isImgLoad: false,
+				showNum: tlscroll,
+				stepLen: tlscroll,
+				time: 3000,
+				btnGo: { left: '#btn-change'}
+			});
+		}else{
+			$("#btn-change").hide();
+		}
 	} 
 }
+
 
 
 $(".sidecategory").hover(function(){
@@ -52,6 +116,21 @@ $('#prdDesc').gLoad(function () {
 	});
 });
 
+/* 一个简易的分页示例 */
+$('.comment-box').gLoad(function () { 
+	gmPrdApp.appraisalPage(1);
+	$('#appraisalPage').gPage({
+		ap: 9,
+		cp: 1,
+		tg: "javascript:;",
+		e: function () {
+			gmPrdApp.appraisalPage(this.cp);
+			gmPrdApp.jumpToAppTop();
+		}
+	});
+});
+
+
 /** 排行榜 **/
 $('#paiHangBang').gLoad(function () { 
 	new Tabs({
@@ -75,3 +154,17 @@ $('#paiHangBang').gLoad(function () {
 	});
 });		
 
+
+//根据浏览猜你喜欢
+$('#guessLike').gLoad(function () { 
+    var ajaxUrl='http://bigd.gome.com.cn/gome/rec?callback=guessLick_45316876843571&boxid=box37&pid=9134032698&area=11010200&cid=148731113814702064&uid=73179590366&imagesize=130&brid=10000010&shopid=&c1id=cat10000004&c3id=cat10000049&sid=1123030306&_=1487757103321';
+	$.ajax({
+		type : "get",
+		url : ajaxUrl,
+		dataType : "jsonp",
+		//data : {},
+		jsonpName : "guessLick_45316876843571"
+	}).done(function(data){
+        gmPrdApp.guessLickFn(data);
+    });
+});
